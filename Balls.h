a615d5940eb2859d79ae2c7ee280c810fec7cab9
EthanOnXPS <ethanzhou76@gmail.com>
2023-05-03 09:09:16 -0400

    Working on final function for balls

--- Balls.h
+++ Balls.h
@@ -6,27 +6,35 @@
 using namespace std;
 
 static unordered_map<string, float> ballsCatalogue{
-  {"Basketball", 49.99},
-  {"Volleyball", 29.99},
-  {"Tennis balls (4x pack)", 26.99},
-  {"Golf balls (12x pack)", 24.99},
-  {"Soccer ball", 34.99}
+  {"Basketball (pairs)", 49.99},
+  {"Volleyball (pairs)", 29.99},
+  {"Tennis balls (pairs)", 26.99},
+  {"Golf balls (pairs)", 24.99},
+  {"Soccer ball (pairs)", 34.99}
 };
 
 static unordered_map<int, string> ballsBarcodes {
-  {1234567, "Basketball"},
-  {1234567, "Volleyball"},
-  {1234567, "Tennis balls (4x pack)"},
-  {1234567, "Golf balls (12x pack)"},
-  {1234567, "Soccer ball"}
+  {1234567, "Basketball (pairs)"},
+  {1234567, "Volleyball (pairs)"},
+  {1234567, "Tennis balls (pairs)"},
+  {1234567, "Golf balls (pairs)"},
+  {1234567, "Soccer ball (pairs)"}
+};
+
+static unordered_map<string, int> barcodeMap{
+  {"Basketball (pairs)", 1234567},
+  {"Volleyball (pairs)", 1234567},
+  {"Tennis balls (pairs)", 1234567},
+  {"Golf balls (pairs)", 1234567},
+  {"Soccer ball (pairs)", 1234567}
 };
 
 static unordered_map<string, int> ballsBought {
-  {"Basketball", 0},
-  {"Volleyball", 0},
-  {"Tennis balls (4x pack)", 0},
-  {"Golf balls (12x pack)", 0},
-  {"Soccer ball", 0}
+  {"Basketball (pairs)", 0},
+  {"Volleyball (pairs)", 0},
+  {"Tennis balls (pairs)", 0},
+  {"Golf balls (pairs)", 0},
+  {"Soccer ball (pairs)", 0}
 };
 
 void printBallsCatalogue(){
@@ -50,4 +58,49 @@ void buyBall(int barcodeNumber){
 bool containsProduct(int barcode){
   auto pointer = ballsBarcodes.find(barcode);
   return pointer == ballsBarcodes.end();
+}
+
+void printCart(){
+  cout << "*** YOUR CART ***" << endl;
+  for (const auto&pair : ballsCatalogue){
+    if (ballsBought[pair.first] > 0){
+      cout << setw(36) << left << pair.first << "  x  " << ballsBought[pair.first] << " @ $" << pair.second << " each" << endl;
+    }
+  }
+}
+
+void printReceipt(){
+  cout << "*** RECEIPT ***\nBALLS" << endl;
+  cout << "************************************" << endl;
+  float subTotal = 0;
+  for (const auto&pair : ballsCatalogue){
+    if (ballsBought[pair.first] > 0){
+      float productTotal = ballsBought[pair.first] * pair.second;
+      cout << pair.first << "\t" << barcodeMap[pair.first] << endl;
+      cout << setw(36) << left << "\t\t" << ballsBought[pair.first] << " @ " << pair.second;
+      cout << right << setprecision(2) << productTotal << endl;
+      subTotal += productTotal;
+    }
+  }
+  cout << setw(36) << left << "Subtotal";
+  cout << right << setprecision(2) << subTotal << endl;
+  cout << setw(36) << left << "Tax (13%)";
+  cout << right << setprecision(2) << subTotal*0.13 << endl;
+  cout << setw(36) << left << "Total";
+  cout << right << setprecision(2) << subTotal*1.13 << endl;
+
+  float payment;
+  do{
+    cout << "Please enter payment amount: ";
+    cin >> payment;
+    if(payment < subTotal*1.13){
+      cout << "ERROR: That is insufficient funds.\n" << endl;
+    }
+  } while (payment < subTotal*1.13);
+}
+
+void ballsStore(){
+  cout << "Welcome to Ethan's Balls\n" << endl;
+  printBallsCatalogue();
+  
 }
